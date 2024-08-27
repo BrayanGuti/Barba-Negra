579cd43f411dc79182f1cdb2119be1f7de4d4d21
BrayanGuti <Brag.863@gmail.com>
2024-08-26 23:50:36 -0500

    All working

---
+++ src/SendPage/SendPageRoot.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SendPage from './SendPage.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SendPage />
+  </React.StrictMode>
+)
